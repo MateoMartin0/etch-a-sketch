6af900e3644156d14fa921157e8c08b96f5e92ef
mateo <mateomartin1503@gmal.com>
2026-09-01 19:31:07 +0000

    Get the main elements from the html file

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
 let drawArea = document.querySelector("#draw-area");
 let buttons = document.getElementsByClassName("choice-btn");
-
+let sizeDisplay = document.getElementsByClassName("size-display");
+let sizeBar = document.getElementsByClassName("size-bar");
