7b97a72555a8bf80996fc427692a1731972671b4
mateo <mateomartin1503@gmal.com>
2026-09-01 20:36:58 +0000

    Create a function that listen the slize bar, remove the previous divs and create news

--- js/main.js
+++ js/main.js
@@ -1,4 +1,20 @@
 let drawArea = document.querySelector("#draw-area");
-let buttons = document.getElementsByClassName("choice-btn");
-let sizeDisplay = document.getElementsByClassName("size-display");
-let sizeBar = document.getElementsByClassName("size-bar");
+let buttons = document.querySelectorAll(".choice-btn");
+let sizeDisplay = document.querySelector(".size-display");
+let sizeBar = document.querySelector(".size-bar");
+
+sizeBar.addEventListener("input", function () {
+    let value = sizeBar.value;
+    let currentSize = value * value;
+
+    let divsToRemove = drawArea.children.length;
+    while (divsToRemove > 0) {
+        drawArea.removeChild(drawArea.children[0]);
+        divsToRemove -= 1;
+    }
+
+    for (let i = 1; i <= currentSize; i++){
+        let divForGrid = document.createElement("div");
+        drawArea.appendChild(divForGrid);
+    }
+})
